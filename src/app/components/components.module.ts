c009830e608006a2849895615c17ca3ca439ad4c
santiago Grossi <santiago.g.grossi@gmail.com>
2026-01-01 23:06:17 -0300

    angular 16

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { FormsModule } from "@angular/forms";
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NgbModule,FormsModule],
+  imports: [CommonModule, RouterModule,FormsModule, NgbCollapseModule],
   declarations: [FooterComponent, NavbarComponent, SidebarComponent],
   exports: [FooterComponent, NavbarComponent, SidebarComponent]
 })
